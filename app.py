067dcf51d71ad66abb794b3db8b668de4eba5748
UtkarshSrivastava <sinhasubbu456@gmail.com>
2018-08-08 16:36:22 +0530

    OauthConfig

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
+import os
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from requests_oauthlib import OAuth2Session
+import requests
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
@@ -9,8 +12,11 @@ from forms import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myblog.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'S3CR3TK3Y!'
+app.config['GOOGLE_CLIENT_ID'] = ''
+app.config['GOOGLE_CLIENT_SECRET'] = ''
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 bootstrap = Bootstrap(app)
 
 db = SQLAlchemy(app)
@@ -25,6 +31,15 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
     posts = db.relationship('BlogPost', backref='user', lazy=True)
+    oauth_id = db.relationship('OauthUser', uselist=False)
+
+
+class OauthUser(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class BlogPost(db.Model):
@@ -40,6 +55,25 @@ class BlogPost(db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+def login_oauth_user():
+    user_info = session.get('user')
+    if user_info:
+        try:
+            name = user_info.get('name')
+            email = user_info.get('email')
+            password = 'GoogleUserLogin1234'
+            user_id = user_info.get('id')
+            hashed_password = generate_password_hash(password, method='sha256')
+            new_user = OauthUser(username=name, email=email, password=hashed_password, user_id=user_id)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('index'))
+        except IntegrityError as exp:
+            db.session.rollback()
+            print(exp)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -128,5 +162,93 @@ def addpost():
     db.session.commit()
     return redirect(url_for("index"))
 
+
+@app.route('/auth', defaults={'action': 'login'})
+@app.route('/auth/<action>')
+def auth(action):
+    if not request.args.get('state'):
+        session['last'] = request.referrer or url_for('index')
+        if 'next' in request.args:
+            session['next'] = url_for(request.args['next'])
+        else:
+            session['next'] = session['last']
+
+    # User logged in, refresh
+    if session.get('user') and action == 'refresh':
+        if 'refresh_token' not in session['user']['token']:
+            flash('Could not refresh, token not present', 'danger')
+            return redirect(session['last'])
+        google = OAuth2Session(
+          app.config['GOOGLE_CLIENT_ID'],
+          token=session['user']['token']
+        )
+        session['user']['token'] = google.refresh_token(
+          'https://accounts.google.com/o/oauth2/token',
+          client_id=app.config['GOOGLE_CLIENT_ID'],
+          client_secret=app.config['GOOGLE_CLIENT_SECRET']
+        )
+        flash('Token refreshed', 'success')
+        return redirect(session['next'])
+
+    # User loggedin - logout &/or revoke
+    if session.get('user'):
+        if action == 'revoke':
+            response = requests.get(
+              'https://accounts.google.com/o/oauth2/revoke',
+              params={'token': session['user']['token']['access_token']}
+            )
+            if response.status_code == 200:
+                flash('Authorization revoked', 'warning')
+            else:
+                flash('Could not revoke token: {}'.format(response.content), 'danger')
+        if action in ['logout', 'revoke']:
+            del session['user']
+            flash('Logged out', 'success')
+        return redirect(session['last'])
+
+    google = OAuth2Session(
+      app.config['GOOGLE_CLIENT_ID'],
+      scope=[
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+      ],
+      redirect_uri=url_for('auth', _external=True),
+      state=session.get('state')
+    )
+
+    # Initial client request, no `state` from OAuth redirect
+    if not request.args.get('state'):
+        url, state = google.authorization_url(
+          'https://accounts.google.com/o/oauth2/auth',
+          access_type='offline'
+        )
+        session['state'] = state
+        return redirect(url)
+
+    # Error returned from Google
+    if request.args.get('error'):
+        error = request.args['error']
+        if error == 'access_denied':
+            error = 'Not logged in'
+        flash('Error: {}'.format(error), 'danger')
+        return redirect(session['last'])
+
+    # Redirect from google with OAuth2 state
+    token = google.fetch_token(
+      'https://accounts.google.com/o/oauth2/token',
+      client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+      authorization_response=request.url
+    )
+    user = google.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+    user['token'] = token
+    session['user'] = user
+    flash('Logged in', 'success')
+    print(227, session)
+    if action == 'login':
+        login_oauth_user()
+    return redirect(session['next'])
+
+
 if __name__ == '__main__':
-    app.run(port=80, debug=True)
+    db.create_all()
+    app.run(port=5000, debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#oauth-btn').click(function(){
+        alert('Class Oriented');
+        window.location = '/auth'
+    })
+});
